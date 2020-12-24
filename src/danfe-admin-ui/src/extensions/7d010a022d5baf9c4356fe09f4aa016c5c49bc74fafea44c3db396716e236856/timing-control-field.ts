7b271036a14fe9951c9d36249049cb33973aad29
Beep Love <biplabkarki04@gmail.com>
2020-12-24 19:50:35 +0545

    somemore

--- src/danfe-admin-ui/src/extensions/7d010a022d5baf9c4356fe09f4aa016c5c49bc74fafea44c3db396716e236856/timing-control-field.ts
+++ src/danfe-admin-ui/src/extensions/7d010a022d5baf9c4356fe09f4aa016c5c49bc74fafea44c3db396716e236856/timing-control-field.ts
@@ -1,8 +1,9 @@
 import { NgModule, Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { SharedModule, CustomFieldControl, 
-  CustomFieldConfigType, registerCustomFieldComponent } from '@vendure/admin-ui/core';
-import { Subscription } from 'rxjs/internal/Subscription';
+import { UserInputError } from '@vendure/core';
+import { SharedModule, CustomFieldControl, CustomFieldConfigType, registerCustomFieldComponent } from '@vendure/admin-ui/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 
 @Component({
@@ -16,69 +17,109 @@ import { Subscription } from 'rxjs/internal/Subscription';
 	<br/><br/>
 	<input formControlName="fromTime" type="time"> -
     <input formControlName="toTime" type="time">
-   
+    <br/><br/>
+    <p *ngIf="error"><small>Opening Time later than the Closing Time is unacceptable.</small></p>	
    </form>
    `
 })
 
 
-export class TimingControl implements CustomFieldControl,OnInit,OnDestroy {
+export class TimingControl implements CustomFieldControl {
   readonly: boolean;
   config: CustomFieldConfigType;
   formControl: FormControl;
   week: any = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-  subs: Subscription;
-  datas = new Array();
-  
-  timeForm = new FormGroup({
+  destroy$ = new Subject();
+  timeForm: FormGroup;
+  data = new Array();
+  error = false;
+    
+  constructor(){
+   
+   this.timeForm = new FormGroup({
 	 weekDay: new FormControl(''),
      fromTime: new FormControl(''),
 	 toTime: new FormControl('')
    });
-
   
+  
+  
+  }
+
   ngOnInit() {
 	
-	//console.log(this.formControl.value);
-	
-    if(!this.formControl.value){
-	  this.formControl.setValue(JSON.stringify({
+    if(!this.formControl.value || this.formControl.value.length==0){
+	  this.formControl.patchValue({
 	    weekDay: "",
 		fromTime: "",
 		toTime: ""
-	  }));
+	  });
+	  this.timeForm.patchValue(this.formControl.value);
+	}else{
+	  let ctime = this.formControl.value;
+	  let time = this.formControl.value.split("-");
+	  this.formControl.patchValue({
+	    weekDay: time[0],
+		fromTime: time[1],
+		toTime: time[2]
+	  });
+	  this.timeForm.patchValue(this.formControl.value);
+	  this.formControl.patchValue(ctime);
 	}
 	
-	this.timeForm.setValue(this.formControl.value);
-	 
-    this.timeForm.valueChanges.subscribe((value) => {
-		if(this.checknotempty(value)){
-		   this.datas.push(value);
+	
+    this.timeForm.valueChanges.pipe(
+	  takeUntil(this.destroy$)
+	).subscribe((value) => {
+		if(this.checkallfields(value)){
+		  this.formControl.patchValue(this.convertToString(value));
 		}
-		this.formControl.setValue(this.datas.toString());
-    });	
-  
+    });
+
+
  }
  
- checknotempty(object){
-  let i=0;
-  Object.keys(object).forEach(function (key) {
-    if (object[key] === '') {
-      i++;
-    }
-  });
-  
-  if(i>=3){
-    return false;
-  }
-  
-  return true;
- 
+ convertToString(object){
+    let data = object["weekDay"]+"-"+object["fromTime"]+"-"+object["toTime"];
+	return data;
  }
+ 
+ checkallfields(object){
+   
+   if(object["fromTime"].length!=0 && object["toTime"].length!=0 && object["weekDay"].length!=0){
+     let t1sec = this.toseconds(object["fromTime"]);
+     let t2sec = this.toseconds(object["toTime"]);
+     let diff = t2sec-t1sec;
+   
+     if(diff < 0){
+	  //throw new UserInputError("Either of the inputs have opening time later than the closing time.");  
+	  //alert("Either of the inputs have opening time later than the closing time.");
+	  this.error = true;
+	  return false;
+	 }
+	 
+	 this.error = false;
+	 return true;
+   
+   }else{
+     return false;
+   }
+   
+   
+ }
+ 
+ toseconds(time){
+    let a = time.split(":");
+	console.log(a);
+	let seconds = Number(a[0])*60*60+Number(a[1])*60;
+	console.log(seconds);
+	return seconds;
+ }
+ 
  
  ngOnDestroy(){
-     //this.formControl.reset();
-	 //this.subs.unsubscribe();
+	this.destroy$.next();
+    this.destroy$.complete();
  }
   
 }
--- src/plugins/timing-control-field.ts
+++ src/plugins/timing-control-field.ts
@@ -1,8 +1,9 @@
 import { NgModule, Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { SharedModule, CustomFieldControl, 
-  CustomFieldConfigType, registerCustomFieldComponent } from '@vendure/admin-ui/core';
-import { Subscription } from 'rxjs/internal/Subscription';
+import { UserInputError } from '@vendure/core';
+import { SharedModule, CustomFieldControl, CustomFieldConfigType, registerCustomFieldComponent } from '@vendure/admin-ui/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 
 @Component({
@@ -16,71 +17,103 @@ import { Subscription } from 'rxjs/internal/Subscription';
 	<br/><br/>
 	<input formControlName="fromTime" type="time"> -
     <input formControlName="toTime" type="time">
-   
+    <p *ngIf="error"><small>Opening Time later than the Closing Time is unacceptable.</small></p>	
    </form>
    `
 })
 
 
-export class TimingControl implements CustomFieldControl,OnInit,OnDestroy {
+export class TimingControl implements CustomFieldControl {
   readonly: boolean;
   config: CustomFieldConfigType;
   formControl: FormControl;
   week: any = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-  subs: Subscription;
-  datas = new Array();
-  
-  timeForm = new FormGroup({
+  destroy$ = new Subject();
+  timeForm: FormGroup;
+  error = false;
+    
+  constructor(){
+   
+   this.timeForm = new FormGroup({
 	 weekDay: new FormControl(''),
      fromTime: new FormControl(''),
 	 toTime: new FormControl('')
    });
-
   
+  
+  
+  }
+
   ngOnInit() {
 	
-	//console.log(this.formControl.value);
-	
-    if(!this.formControl.value){
-	  this.formControl.setValue(JSON.stringify({
+    if(!this.formControl.value || this.formControl.value.length==0){
+	  this.formControl.patchValue({
 	    weekDay: "",
 		fromTime: "",
 		toTime: ""
-	  }));
+	  });
+	  this.timeForm.patchValue(this.formControl.value);
 	}else{
-	   
+	  let ctime = this.formControl.value;
+	  let time = this.formControl.value.split("-");
+	  this.formControl.patchValue({
+	    weekDay: time[0],
+		fromTime: time[1],
+		toTime: time[2]
+	  });
+	  this.timeForm.patchValue(this.formControl.value);
+	  this.formControl.patchValue(ctime);
 	}
 	
-	this.timeForm.setValue(this.formControl.value);
-	 
-    this.timeForm.valueChanges.subscribe((value) => {
-		if(this.checknotempty(value)){
-		   this.datas.push(JSON.stringify(value));
+	
+    this.timeForm.valueChanges.pipe(
+	  takeUntil(this.destroy$)
+	).subscribe((value) => {
+		if(this.checkallfields(value)){
+		  this.formControl.patchValue(this.convertToString(value));
 		}
-		this.formControl.setValue(this.datas.toString());
-    });	
-  
+    });
+
+
  }
  
- checknotempty(object){
-  let i=0;
-  Object.keys(object).forEach(function (key) {
-    if (object[key] === '') {
-      i++;
-    }
-  });
-  
-  if(i>=3){
-    return false;
-  }
-  
-  return true;
- 
+ convertToString(object){
+    let data = object["weekDay"]+"-"+object["fromTime"]+"-"+object["toTime"];
+	return data;
  }
+ 
+ checkallfields(object){
+   
+   if(object["fromTime"].length!=0 && object["toTime"].length!=0 && object["weekDay"].length!=0){
+     let t1sec = this.toseconds(object["fromTime"]);
+     let t2sec = this.toseconds(object["toTime"]);
+     let diff = t2sec-t1sec;
+   
+     if(diff < 0){
+	  this.error = true;
+	  return false;
+	 }
+	 
+	 this.error = false;
+	 return true;
+   
+   }else{
+     return false;
+   }
+   
+   
+ }
+ 
+ toseconds(time){
+    let a = time.split(":");
+	let seconds = Number(a[0])*60*60+Number(a[1])*60;
+	return seconds;
+ }
+ 
  
  ngOnDestroy(){
-     //this.formControl.reset();
-	 //this.subs.unsubscribe();
+	this.destroy$.next();
+    this.destroy$.complete();
  }
   
 }
