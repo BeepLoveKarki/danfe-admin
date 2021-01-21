1ed44ffc27b5b62132788abb114ece2679025ec2
Beep Love <biplabkarki04@gmail.com>
2021-01-21 11:33:54 +0545

    same

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -54,6 +54,8 @@ import { ConnectIPSPlugin } from './payment-gateways/connectips/connectips-token
 //import { OnePerOrder } from './promotion-strategies/one-per-order';
 
 import { ChannelsPlugin } from './plugins/channels/channels-plugin';
+
+
 import { ProductExtensionPlugin } from './plugins/channels/product-extension-plugin';
 import { ProductVariantExtensionPlugin } from './plugins/channels/product-variant-extension-plugin';
 
@@ -223,6 +225,7 @@ apiOptions: {
 		VendorPlugin,
 		FeedbackPlugin,
 		ChannelsPlugin,
+		
 		ProductExtensionPlugin,
 		ProductVariantExtensionPlugin,
 		//BraintreePlugin,
